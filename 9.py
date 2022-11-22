9d4a2394d7edd8214d0c401d36d4bf6f67cfb9f3
milvus <thommy.python.dev@gmail.com>
2022-11-22 10:56:12 +0300

    2 task

--- 9.py
+++ 9.py
@@ -1,25 +1,38 @@
-from random import randint, shuffle
+# from random import randint, shuffle
+#
+#
+# class Student:
+#
+#     def __init__(self, first_name: str, group: int, marks: list[int]) -> None:
+#         self.first_name = first_name
+#         self.group = group
+#         self.marks = marks
+#
+#     def __str__(self) -> str:
+#         return f'Name: {self.first_name.title()}, Group: {self.group}, Marks: {self.marks}'
+#
+#
+# def student_sort(students: list[Student]) -> list[Student]:
+#     return sorted(students, key=lambda student: student.first_name)
+#
+#
+# user = Student('Alex', 45, [1, 2, 3])
+# user3 = Student('Ivan', 45, [5, 4, 3])
+# user2 = Student('Eva', 44, [4, 5, 6])
+# students = [user2, user3, user]
+# students = student_sort(students)
+# for student in students:
+#     print(student)
 
 
-class Student:
+class Rectangle:
 
-    def __init__(self, first_name: str, group: int, marks: list[int]) -> None:
-        self.first_name = first_name
-        self.group = group
-        self.marks = marks
+    def __init__(self, x: tuple[int, int], y: tuple[int, int]) -> None:
+        self.width = abs(x[0] - y[0])
+        self.height = abs(x[1] - y[1])
 
-    def __str__(self) -> str:
-        return f'Name: {self.first_name.title()}, Group: {self.group}, Marks: {self.marks}'
+    def perimeter(self) -> int:
+        return (self.width + self.height) * 2
 
-
-def student_sort(students: list[Student]) -> list[Student]:
-    return sorted(students, key=lambda student: student.first_name)
-
-
-user = Student('Alex', 45, [1, 2, 3])
-user3 = Student('Ivan', 45, [5, 4, 3])
-user2 = Student('Eva', 44, [4, 5, 6])
-students = [user2, user3, user]
-students = student_sort(students)
-for student in students:
-    print(student)
+    def square(self) -> int:
+        return self.width * self.height
