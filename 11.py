933948788b7de88e95ccbaa683720d01e6f06315
milvus <thommy.python.dev@gmail.com>
2022-11-29 11:24:49 +0300

    category

--- 11.py
+++ 11.py
@@ -41,51 +41,75 @@ class Taxi(object):
 # print(taxi.find_car(4, False))
 # print(taxi.find_car(4, False))
 
+# class Category(object):
+#
+#     categories: list[str] = []
+#
+#     @classmethod
+#     def add(cls, category: str) -> int:
+#         category = category.title()
+#         if category in cls.categories:
+#             raise ValueError('category is  not unique')
+#         cls.categories.append(category)
+#         return cls.categories.index(category)
+#         # return len(cls.categories) - 1
+#
+#     @classmethod
+#     def get(cls, pk: int) -> str:
+#         try:
+#             return cls.categories[pk]
+#         except IndexError as e:
+#             raise ValueError(e)
+#
+#     @classmethod
+#     def delete(cls, pk: int) -> None:
+#         try:
+#             del cls.categories[pk]
+#         except IndexError:
+#             ...  # pass
+#
+#     # @classmethod
+#     # def update(cls, pk: int, new_category_name: str):
+#     #     new_category_name = new_category_name.title()
+#     #     if new_category_name in cls.categories:
+#     #         raise ValueError('category is not unique')
+#     #     try:
+#     #         cls.get(pk=pk)
+#     #     except ValueError:
+#     #         cls.add(category=new_category_name)
+#     #     else:
+#     #         cls.categories[pk] = new_category_name
+#
+#     @classmethod
+#     def update(cls, pk: int, new_category_name: str):
+#         new_category_name = new_category_name.title()
+#         if new_category_name in cls.categories:
+#             raise ValueError('category is not unique')
+#         try:
+#             cls.categories[pk] = new_category_name
+#         except IndexError:
+#             cls.add(category=new_category_name)
+
+
 class Category(object):
 
-    categories: list[str] = []
+    categories: list[dict] = []
 
     @classmethod
-    def add(cls, category: str) -> int:
-        category = category.title()
-        if category in cls.categories:
-            raise ValueError('category is  not unique')
-        cls.categories.append(category)
-        return cls.categories.index(category)
-        # return len(cls.categories) - 1
-
-    @classmethod
-    def get(cls, pk: int) -> str:
-        try:
-            return cls.categories[pk]
-        except IndexError as e:
-            raise ValueError(e)
-
-    @classmethod
-    def delete(cls, pk: int) -> None:
-        try:
-            del cls.categories[pk]
-        except IndexError:
-            ...  # pass
-
-    # @classmethod
-    # def update(cls, pk: int, new_category_name: str):
-    #     new_category_name = new_category_name.title()
-    #     if new_category_name in cls.categories:
-    #         raise ValueError('category is not unique')
-    #     try:
-    #         cls.get(pk=pk)
-    #     except ValueError:
-    #         cls.add(category=new_category_name)
-    #     else:
-    #         cls.categories[pk] = new_category_name
-
-    @classmethod
-    def update(cls, pk: int, new_category_name: str):
-        new_category_name = new_category_name.title()
-        if new_category_name in cls.categories:
+    def add(cls, category_name: str, is_published: bool) -> int:
+        category_name = category_name.title()
+        category = tuple(filter(lambda x: x['name'] == category_name, cls.categories))
+        if category:
             raise ValueError('category is not unique')
-        try:
-            cls.categories[pk] = new_category_name
-        except IndexError:
-            cls.add(category=new_category_name)
+        cls.categories.append(
+            {
+                'name': category_name,
+                'is_published': is_published
+            }
+        )
+        return len(cls.categories) - 1
+
+
+print(Category.add('Food', True))
+print(Category.add('Drink', True))
+print(Category.add('food', False))
